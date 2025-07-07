e783dbfe8910ef21ea8b15db95bf72b272606668
romanlusnikov <aectann28@gmail.com>
2025-07-07 15:59:05 +0400

    + НФ

--- menu-service/src/main/java/menuservice/MenuController.java
+++ menu-service/src/main/java/menuservice/MenuController.java
@@ -1,5 +1,6 @@
 package menuservice;
 
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -21,5 +22,21 @@ public class MenuController {
     @GetMapping("/{id}")
     public Menu getMenuItem(@PathVariable Long id) {
         return menuRepository.findById(id).orElseThrow();
+    }
+
+    @PostMapping
+    public Menu addMenuItem(@RequestBody Menu menuItem) {
+        return menuRepository.save(menuItem);
+    }
+    @GetMapping("/add-form")
+    public String showForm(Model model) {
+        model.addAttribute("menu", new Menu());
+        return "add-menu-item";
+    }
+
+    @PostMapping("/add")
+    public String addItem(@ModelAttribute Menu item) {
+        menuRepository.save(item);
+        return "redirect:/menu";
     }
 }
